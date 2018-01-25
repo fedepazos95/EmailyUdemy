8fed82bc65443cc3923a75975cefe3735b8c273a
Federico Pazos <federico.pazos@levinglobal.com>
2018-01-25 17:46:38 -0300

    introducing stripe

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
   renderContent() {
@@ -13,18 +15,24 @@ class Header extends Component {
           </li>
         );
       default:
-        return (
-          <li>
-            <a>Logout</a>
+        return [
+          <li key="1"><Payments /></li>,
+          <li key="2">
+            <a href="/api/logout">Logout</a>
           </li>
-        );
+        ];
     }
   }
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">Emaily</a>
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className="left brand-logo"
+          >
+            Emaily
+          </Link>
           <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
